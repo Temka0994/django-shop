1e9950455d6a7233858225e396b498d1333fc15a
Temka0994 <pleskovartem878@gmail.com>
2024-10-16 22:38:39 +0300

    added catalog and product urls

--- MyDjangoShop/urls.py
+++ MyDjangoShop/urls.py
@@ -1,26 +1,16 @@
-"""
-URL configuration for MyDjangoShop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""URL configuration for MyDjangoShop project."""
 from django.contrib import admin
 from django.urls import path
 import home.views
 import about.views
+from catalog.views import ProductListView, ProductDetailView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.home, name='home'),
-    path('about/', about.views.about, name="about")
-]
+    path('about/', about.views.about, name="about"),
+    path('catalog/', ProductListView.as_view(), name="catalog"),
+    path('product/<slug:product_slug>/', ProductDetailView.as_view(), name="product_details"),
+] + static(settings.MEDIA_URL)
