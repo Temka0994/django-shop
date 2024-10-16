cc0b75b8cba9d452d29887fbce59d6bd1f98ce15
Temka0994 <pleskovartem878@gmail.com>
2024-10-16 22:39:31 +0300

    created product page and updated catalog page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
+from django.views import generic
 
-# Create your views here.
+
+class ProductListView(generic.ListView):
+    model = Product
+    context_object_name = 'product_list'
+    template_name = 'catalog/catalog.html'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    slug_field = 'slug'
+    slug_url_kwarg = 'product_slug'
+    context_object_name = 'product'
+    template_name = 'catalog/product_details.html'
+
