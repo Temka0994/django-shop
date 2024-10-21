c85b3d14472ac0cb91bab8162ce70447efc7ab5f
Temka0994 <pleskovartem878@gmail.com>
2024-10-21 20:22:10 +0300

    created registration and login

--- MyDjangoShop/urls.py
+++ MyDjangoShop/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 import home.views
 import about.views
+import personalAccount.views
 from catalog.views import ProductListView, ProductDetailView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,4 +14,8 @@ urlpatterns = [
     path('about/', about.views.about, name="about"),
     path('catalog/', ProductListView.as_view(), name="catalog"),
     path('product/<slug:product_slug>/', ProductDetailView.as_view(), name="product_details"),
+    path('login/', personalAccount.views.login_view, name="login"),
+    path('registration/', personalAccount.views.registration_view, name="registration"),
+    path('login/', personalAccount.views.logout_view, name="logout"),
+    path('cabinet/', personalAccount.views.cabinet, name="cabinet"),
 ] + static(settings.MEDIA_URL)
--- personalAccount/views.py
+++ personalAccount/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as user_login, logout as logout_view
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            user_login(request, user)
+            return redirect('catalog')
+    return render(request, 'personalAccount/login.html')
+
+
+def registration_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+
+        if password == password_confirmation:
+            user = User.objects.create_user(username=username, password=password)
+            user_login(request, user)
+            return redirect('catalog')
+    return render(request, 'personalAccount/registration.html')
+
+
+def cabinet(request):
+    return render(request, 'personalAccount/cabinet.html')
+
+
+def logout_view(request):
+    return render(request, 'personalAccount/logout.html')
